173c872c0d53d1e31e5255ccc2fc3d4d3e37fb72
James Yeates <jimibue@gmail.com>
2020-11-17 11:41:29 -0700

    adds note model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_175235) do
+ActiveRecord::Schema.define(version: 2020_11_17_182129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,13 @@ ActiveRecord::Schema.define(version: 2020_11_17_175235) do
     t.boolean "friend"
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.text "body"
+    t.bigint "contact_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["contact_id"], name: "index_notes_on_contact_id"
+  end
+
+  add_foreign_key "notes", "contacts"
 end
